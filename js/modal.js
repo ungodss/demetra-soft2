0ff4b3f6b28ed9c3c4a87b42476a506f79279964
Sergey <ssolodchuk@inbox.ru>
2024-02-15 02:55:37 +0300

    project krivoi flex

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,5 @@
+
+//модальное окно
 const modalController = ({modal, btnClose, btnOpen}) => {
 const buttonElems = document.querySelectorAll(btnOpen);
 const modalElem = document.querySelector(modal);
@@ -39,6 +41,8 @@ modalController({
     btnClose:'.modal__close'
 });
 
+
+//о компании слайдер
 const galleryContainer = document.querySelector('.gallery-container');
 const galleryControlsContainer = document.querySelector('.gallery-controls');
 const galleryControls = ['previous', 'next'];
@@ -97,3 +101,55 @@ class Carousel {
 const exampleCarousel = new Carousel(galleryContainer, galleryItems, galleryControls);
 exampleCarousel.setControls();
 exampleCarousel.useControls();
+
+
+//слайдер проектов
+( function($) {
+  
+    $(document).ready(function() {
+      
+      var s           = $('.slider'),
+          sWrapper    = s.find('.slider-wrapper'),
+          sItem       = s.find('.slide'),
+          btn         = s.find('.slider-link'),
+          sWidth      = sItem.width(),
+          sCount      = sItem.length,
+          slide_date  = s.find('.slide-date'),
+          slide_title = s.find('.slide-title'),
+          slide_text  = s.find('.slide-text'),
+          slide_more  = s.find('.slide-more'),
+          slide_image = s.find('.slide-image img'),
+          sTotalWidth = sCount * sWidth;
+      
+      sWrapper.css('width', sTotalWidth);
+      sWrapper.css('width', sTotalWidth);
+      
+      var clickCount  = 0;
+      
+      btn.on('click', function(e) {
+        e.preventDefault();
+  
+        if( $(this).hasClass('next') ) {
+          
+          ( clickCount < ( sCount - 1 ) ) ? clickCount++ : clickCount = 0;
+        } else if ( $(this).hasClass('prev') ) {
+          
+          ( clickCount > 0 ) ? clickCount-- : ( clickCount = sCount - 1 );
+        }
+        TweenMax.to(sWrapper, 0.4, {x: '-' + ( sWidth * clickCount ) })
+  
+  
+        //CONTENT ANIMATIONS
+  
+        var fromProperties = {autoAlpha:0, x:'-50', y:'-10'};
+        var toProperties = {autoAlpha:0.8, x:'0', y:'0'};
+  
+        TweenLite.fromTo(slide_image, 1, {autoAlpha:0, y:'40'}, {autoAlpha:1, y:'0'});
+        TweenLite.fromTo(slide_date, 0.4, fromProperties, toProperties);
+  
+      });
+            
+    });
+  })(jQuery);
+  
+  $('.overlay').addClass('overlay-blue');
